b8bc7aaccc5c6c3e55b45158669661bdb173e977
Preet Vadaliya <preetvadaliya@gmail.com>
2024-08-06 15:25:13 +0200

    Update App component to include MUI theme provider, CSS baseline, and SnackbarProvider for improved styling and user experience. Create Router component to handle routing with react-router-dom and integrate Supabase authentication.

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,3 +1,15 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { SnackbarProvider } from 'notistack';
+import { Router } from './Router';
+import { lightTheme } from './helpers';
+
 export function App() {
-  return <div>App</div>;
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline />
+      <SnackbarProvider>
+        <Router />
+      </SnackbarProvider>
+    </ThemeProvider>
+  );
 }
---
+++ src/renderer/src/Router.tsx
@@ -0,0 +1,67 @@
+import { createClient } from '@supabase/supabase-js';
+import { useAtom } from 'jotai';
+import { useEffect, useMemo, useState } from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Supabase } from './helpers';
+import {
+  AuthLayout,
+  Course,
+  Dashboard,
+  ForgotPassword,
+  Home,
+  Loading,
+  LogIn,
+  MainLayout,
+  ResetPassword
+} from './screens';
+
+export function Router() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [supabase, setSupabase] = useAtom(Supabase);
+
+  const router = useMemo(() => {
+    return createBrowserRouter([
+      {
+        path: '/',
+        Component: MainLayout,
+        children: [
+          { path: 'home', Component: Home },
+          { path: 'my-course', Component: Course },
+          { path: 'dashboard', Component: Dashboard }
+        ]
+      },
+      {
+        path: '/',
+        Component: AuthLayout,
+        children: [
+          { path: 'login', Component: LogIn },
+          { path: 'forgot-password', Component: ForgotPassword },
+          { path: 'reset-password', Component: ResetPassword }
+        ]
+      }
+    ]);
+  }, []);
+
+  useEffect(() => {
+    if (supabase) return;
+    const supabaseClient = createClient(
+      process.env.VITE_SUPABASE_URL || '',
+      process.env.VITE_SUPABASE_ANON_KEY || ''
+    );
+    setSupabase(supabaseClient);
+  }, []);
+
+  useEffect(() => {
+    if (!supabase) return;
+    supabase.auth.onAuthStateChange((event, _) => {
+      if (event === 'INITIAL_SESSION' && !_?.user) router.navigate('/login', { replace: true });
+      if (event === 'SIGNED_OUT') router.navigate('/login', { replace: true });
+      if (event === 'SIGNED_IN') router.navigate('/', { replace: true });
+    });
+    setIsLoading(false);
+  }, [supabase]);
+
+  if (isLoading)
+    return <Loading primaryMessage='Please Wait...' secondaryMessage='Getting User Information' />;
+  return <RouterProvider router={router} />;
+}
