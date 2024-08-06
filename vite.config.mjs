ba88ac3a0b2a1bdf7513619b11e85ac2a51f932e
Preet Vadaliya <preetvadaliya@gmail.com>
2024-08-06 15:25:30 +0200

    chore: Update jotai npm dependency to version 2.9.1

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,14 +1,17 @@
-import { join } from 'node:path';
+import { join, resolve } from 'node:path';
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // biome-ignore lint/style/noDefaultExport: <explanation>
 export default defineConfig({
   plugins: [react()],
   root: join(__dirname, 'src', 'renderer'),
+  define: {
+    'process.env': loadEnv('', __dirname)
+  },
   resolve: {
     alias: {
-      '@renderer': join('src', 'renderer', 'src')
+      '@renderer': resolve(join('src', 'renderer', 'src'))
     }
   },
   server: {
